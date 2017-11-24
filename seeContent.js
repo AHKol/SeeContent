1b4498163997145cf4e37601b18f3700dc05c2bb
unknown <ahkolodko@myseneca.ca>
2017-11-24 10:31:35 -0500

    initial commit

--- seeContent.js
+++ seeContent.js
@@ -1,10 +1,23 @@
-function onKeypress(e){ 
-    var ctrlPressed=0;
-    var evt = (e==null ? event:e);
-    ctrlPressed =evt.ctrlKey;
-    if ((ctrlPressed) && (evt.key == 'q')) {
-        console.log("yup" + e.key); 
+function onKeypress(e){
+    if ((e.ctrlKey) && (e.key == 'q' || e.key == 'Q')) {
+        e.preventDefault();
+        console.log(e.key + " pressed"); 
+        openLargest();
     }
 };
+function openLargest() {
+    
+    var images = document.getElementsByTagName("img");
+    var largestImage = images[0];
+    console.log(images.length);
 
+
+    for(let i = 0; i < images.length; i++){
+        if ((images[i].naturalHeight * images[i].naturalWidth) > (largestImage.naturalHeight * largestImage.naturalWidth) )
+            largestImage = images[i];
+    };
+    console.log(largestImage.src);
+    window.open(largestImage.src);
+    
+};
 document.addEventListener("keypress", onKeypress);
