de2c7f0a867139cea1ef6111f473952f8488d98e
unknown <ahkolodko@myseneca.ca>
2017-11-24 10:33:33 -0500

    Removed testing console.logs

--- seeContent.js
+++ seeContent.js
@@ -1,7 +1,6 @@
 function onKeypress(e){
     if ((e.ctrlKey) && (e.key == 'q' || e.key == 'Q')) {
         e.preventDefault();
-        console.log(e.key + " pressed"); 
         openLargest();
     }
 };
@@ -9,14 +8,12 @@ function openLargest() {
     
     var images = document.getElementsByTagName("img");
     var largestImage = images[0];
-    console.log(images.length);
 
 
     for(let i = 0; i < images.length; i++){
         if ((images[i].naturalHeight * images[i].naturalWidth) > (largestImage.naturalHeight * largestImage.naturalWidth) )
             largestImage = images[i];
     };
-    console.log(largestImage.src);
     window.open(largestImage.src);
     
 };
